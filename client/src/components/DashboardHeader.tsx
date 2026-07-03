3e50078489696016ff5cda9849711d9103eece4d
rizqipratama25 <rizqip501@gmail.com>
2026-07-03 08:12:18 +0700

    Test

--- client/src/components/DashboardHeader.tsx
+++ client/src/components/DashboardHeader.tsx
@@ -15,6 +15,7 @@ const DashboardHeader = ({ name, email }: Props) => {
                         <div className="hidden md:block">
                             <p className="text-sm font-semibold">{name}</p>
                             <p className="text-xs text-gray-500">{email}</p>
+                            <p></p>
                         </div>
                     </div>
                 </div>
